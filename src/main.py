0fa66faadd18b29db08319c302b40e8bbe2cf8c3
LemonLime009 <135552380+LemonLime009@users.noreply.github.com>
2023-09-16 22:39:03 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
 from matrix import matrix
+import time
 
 # Initializing display
 display = matrix()
 
-# Drawing rectangle (2x3)
-display.rectangle(2,3)
+# Drawing rectangle (2x3), starting at 2,3
+display.rectangle(2,3,2,3)
 
 # Updating screen
 display.update()
+
+# Waiting 3 seconds
+time.sleep(3)
+
+# Clearing screen
+display.clear()
