52240c68562ec72e166ee4bdc0e1b0c3ce85741e
LemonLime009 <135552380+LemonLime009@users.noreply.github.com>
2023-09-16 22:39:28 -0400

    Update matrix.py

--- src/matrix.py
+++ src/matrix.py
@@ -1,31 +1,35 @@
-class matrix():
+import os
 
-  def __init__(self):
-    self.a = [" ", " ", " ", " ", " "]
-    self.b = [" ", " ", " ", " ", " "]
-    self.c = [" ", " ", " ", " ", " "]
-    self.d = [" ", " ", " ", " ", " "]
-    self.e = [" ", " ", " ", " ", " "]
+def clearFunction():
+  os.system("clear")
 
-  def update(self):
-    print(f"{self.a[0]}{self.a[1]}{self.a[2]}{self.a[3]}{self.a[4]}")
-    print(f"{self.b[0]}{self.b[1]}{self.b[2]}{self.b[3]}{self.b[4]}")
-    print(f"{self.c[0]}{self.c[1]}{self.c[2]}{self.c[3]}{self.c[4]}")
-    print(f"{self.d[0]}{self.d[1]}{self.d[2]}{self.d[3]}{self.d[4]}")
-    print(f"{self.e[0]}{self.e[1]}{self.e[2]}{self.e[3]}{self.e[4]}")
+class Matrix:
 
-  def rectangle(self, length, width):
-    lines = 0
-    while lines != width:
-      lines += 1
-      for i in range(length):
-        if lines == 1:
-          self.a[i] = "O"
-        elif lines == 2:
-          self.b[i] = "O"
-        elif lines == 3:
-          self.c[i] = "O"
-        elif lines == 4:
-          self.d[i] = "O"
-        elif lines == 5:
-          self.e[i] = "O"
+    def __init__(self, width=10, height=10):
+        self.width = width
+        self.height = height
+        self.matrix = [[" " for _ in range(width)] for _ in range(height)]
+
+    def update(self):
+        for row in self.matrix:
+            print("".join(row))
+
+    def rectangle(self, length, width, posX, posY):
+        if (
+            posX < 0
+            or posY < 0
+            or posX + length > self.width
+            or posY + width > self.height
+        ):
+            return False
+
+        for i in range(posX, posX + length):
+            for j in range(posY, posY + width):
+                self.matrix[j][i] = " #"
+
+    def clear(self):
+        for i in range(self.width):
+            for j in range(self.height):
+                self.matrix[j][i] = "  "
+        clearFunction()
+        self.update()
